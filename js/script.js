48224fe92d06e6ff060c70d6cda8f664ee0707a9
nosirjon05 <nosirjonkomilov05@gmail.com>
2022-08-01 17:02:56 +0500

    Add code

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var elForm = document.querySelector(".site-form");
+var elInput = elForm.querySelector(".site-input");
+var elInputtwo = elForm.querySelector(".site-inputtwo");
+var minmoney = 200
+
+var elP = document.querySelector(".text");
+
+elForm.addEventListener("submit", function(evt) {
+   evt.preventDefault();
+
+   if (isNaN(elInputtwo.value)) {
+    elP.textContent = `${elInput.value} Son kiriting`
+  } 
+  else if (elInputtwo.value < minmoney) {
+    elP.textContent = `${elInput.value} Pulingiz yetmadi`
+  }
+  else if(elInputtwo.value >= minmoney){
+    elP.textContent = `${elInput.value} pullingiz yetdi`
+  }
+})
